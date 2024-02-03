8051016d89da83a8ff8d915d9e982f86c182a2b7
Ricardo Gualter <ricardo.gualter@gmail.com>
2024-02-03 15:16:44 -0300

    adjust obdb-pipeline file

--- dags/obdb-pipeline.py
+++ dags/obdb-pipeline.py
@@ -29,14 +29,33 @@ dag = DAG(
 ingest_api_to_s3 = PythonOperator(
     task_id="upload_to_s3_task", python_callable=get_api_request, dag=dag
 )
-raw_to_silver_task = SparkSubmitOperator(
-    task_id="raw_to_silver_task",
-    application="/opt/airflow/etl_files/raw_to_silver.py",
+
+raw_to_bronze_task = SparkSubmitOperator(
+    task_id="raw_to_bronze_task",
+    application="/opt/airflow/etl_files/raw_to_bronze.py",
     conn_id="spark_default",
     conf={
         "spark.hadoop.fs.s3a.endpoint": "s3.amazonaws.com",
-        "spark.driver.extraClassPath": "/opt/airflow/spark/jars/hadoop-aws-3.3.1.jar:/opt/airflow/spark/jars/aws-java-sdk-bundle-1.11.901.jar",
+        "spark.driver.extraClassPath": "/opt/airflow/spark/jars/hadoop-aws-3.3.1.jar:/opt/airflow/spark/jars/aws-java-sdk-bundle-1.11.901.jar:/opt/airflow/spark/jars/delta-core_2.12-2.3.0.jar:/opt/airflow/spark/jars/delta-storage-2.3.0.jar",
         "spark.hadoop.fs.s3a.impl": "org.apache.hadoop.fs.s3a.S3AFileSystem",
+        "spark.sql.catalog.spark_catalog": "org.apache.spark.sql.delta.catalog.DeltaCatalog",
+        "spark.delta.logStore.class":"org.apache.spark.sql.delta.storage.S3SingleDriverLogStore",
+        "spark.jars.packages": "org.apache.hadoop:hadoop-aws:3.3.1",
+        "spark.hadoop.fs.s3a.path.style.access": "true",
+    },
+    dag=dag,
+)
+
+bronze_to_silver_task = SparkSubmitOperator(
+    task_id="bronze_to_silver_task",
+    application="/opt/airflow/etl_files/bronze_to_silver.py",
+    conn_id="spark_default",
+    conf={
+        "spark.hadoop.fs.s3a.endpoint": "s3.amazonaws.com",
+        "spark.driver.extraClassPath": "/opt/airflow/spark/jars/hadoop-aws-3.3.1.jar:/opt/airflow/spark/jars/aws-java-sdk-bundle-1.11.901.jar:/opt/airflow/spark/jars/delta-core_2.12-2.3.0.jar:/opt/airflow/spark/jars/delta-storage-2.3.0.jar",
+        "spark.hadoop.fs.s3a.impl": "org.apache.hadoop.fs.s3a.S3AFileSystem",
+        "spark.sql.catalog.spark_catalog": "org.apache.spark.sql.delta.catalog.DeltaCatalog",
+        "spark.delta.logStore.class":"org.apache.spark.sql.delta.storage.S3SingleDriverLogStore",
         "spark.jars.packages": "org.apache.hadoop:hadoop-aws:3.3.1",
         "spark.hadoop.fs.s3a.path.style.access": "true",
     },
@@ -59,4 +78,4 @@ silver_to_gold_task = SparkSubmitOperator(
     dag=dag,
 )
 
-ingest_api_to_s3 >> raw_to_silver_task >> silver_to_gold_task
+ingest_api_to_s3 >> raw_to_bronze_task >> bronze_to_silver_task >> silver_to_gold_task
